3d3e74be20c3f93838a7a9b3f8028b52b5c9aa13
Eduardo Orellana <123591780+EOrellanaVasquez@users.noreply.github.com>
2023-10-05 14:40:22 -0300

    commit

--- src/js/index.js
+++ src/js/index.js
@@ -1,16 +1,36 @@
 //import react into the bundle
-import React from 'react'
-import {createRoot} from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom";
+import { UserProvider } from "./store/UserContext";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-//include your index.scss file into the bundle
+// include your styles into the webpack bundle
 import "../styles/index.css";
 
 //import your own components
-import Layout from './layout.js'
-
-//
-const root = createRoot(document.querySelector("#app"))
+import Navbar from "./component/Navbar";
+import Home from "./component/Home";
+import CharacterDetail from "./component/CharacterDetail";
+import PlanetDetail from "./component/PlanetDetail";
+import Footer from "./component/Footer";
+import Login from "./component/Login";
+import Signup from "./component/Signup"; // Importa el componente Signup
+import PrivateRoute from "./component/PrivateRoute"
 
 //render your react application
-root.render(<Layout/>)
+ReactDOM.render(
+  <UserProvider>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/home" element={<PrivateRoute element={<Home />} />} />
+        <Route path="/detail_character/:characterId" element={<PrivateRoute element={<CharacterDetail />} />} />
+        <Route path="/detail_planet/:planetId" element={<PrivateRoute element={<PlanetDetail />} />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
 
+    </Router>
+  </UserProvider>,
+  document.querySelector('#app')
+);
